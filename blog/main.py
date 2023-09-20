9f9da424e75d30f10bc2335d4569f1c529b7e0bc
progIfe <ifeoluwasamson90@gmail.com>
2023-09-20 10:41:28 +0100

    Update product endpoint bug fix

--- blog/main.py
+++ blog/main.py
@@ -50,19 +50,22 @@ def get_blog_by_id(id, response: Response, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update_blog_by_id(id, request: Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id of {id} not found')
     
     # update individual blog attributes
-    blog.update(request)
+    for key, value in request.__dict__.items():
+        setattr(blog, key, value)
+    
+    # blog.update(request)
     
     # if request.title:
     #     blog.title = request.title
     # if request.body:
     #     blog.body = request.body
     
-    blog.update(request)
+    # blog.update(request)
     
     db.commit()
     
@@ -72,6 +75,8 @@ def update_blog_by_id(id, request: Blog, db: Session = Depends(get_db)):
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog_by_id(id, request: Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog with the id of {id} not found')
     blog.delete(synchronize_session=False)
     
     db.commit()
