702710c43df207e44d2af81542b815de67e67386
progIfe <ifeoluwasamson90@gmail.com>
2023-09-25 21:38:08 +0100

    User login endpoint and router

--- blog/hashing.py
+++ blog/hashing.py
@@ -6,4 +6,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     def bcrypt(password: str):
         return pwd_context.hash(password)
+    
+    def verify(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
         
--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
+from .. import models
+from ..database import get_db
 from blog.schemas import Login
+from blog.hashing import Hash
 
 
 router = APIRouter(
@@ -8,5 +12,17 @@ router = APIRouter(
 )
 
 @router.post('/login')
-def login(request: Login):
-    return 'login'
+def login(request: Login, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Ivalid email!'
+        )
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Incorrect password!'
+        )        
+        
+    return user
